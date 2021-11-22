7540ed89a56c075754c6df9bfb54e67e81249624
Shannon Li <38844941+augurnaut@users.noreply.github.com>
2021-11-22 05:04:51 -0500

    moved save_csv to proper file
    copy and pasted from app.py

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -28,3 +28,39 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+# moved save_csv function here to refine system design 
+# starter files also literally says "save csv file"
+    
+def save_csv(qualifying_loans, output_path):
+
+    # function description: saves all of the filtered results locally
+    # args: 
+    #   qualifying_loans (list[lists]): (has the data that needs to be saved)
+    #   ouput_path (string): location of saved csv file 
+    # returns: nothing; csv create in path
+
+    # creating a path
+
+    csvpath = Path(output_path)
+
+    # naming the header.
+
+    header = ["bank_data", "credit_score", "debt", "income", "loan_amount", "home_value"] 
+
+    # write the list of lists as a csv
+
+    with open(csvpath, "w", newline='') as csvfile:
+
+        # create a csvwriter
+
+        csvwriter = csv.writer(csvfile, delimiter=",")
+
+        # write the header to the csv file
+
+        csvwriter.writerow(header)
+
+        # write the data rows
+
+        for row in qualifying_loans:
+                csvwriter.writerow(row)
